4420e3a58688e8defedf53e9b08fb39ad0f02d02
tom-bourneil <bourneiltom1@gmail.com>
2025-11-18 15:26:59 +0100

    Update create_3_types_of_units.py

--- create_3_types_of_units.py
+++ create_3_types_of_units.py
@@ -3,53 +3,53 @@ import random
 
 class Unit:
     
-    def __init__(self, nom, team, hp, armure, attaque, portee, size, vue, vitesse, precision, rechargement, temps_recharg, x, y, order):
-        self.nom =nom 
+    def __init__(self, name, team, hp, armor, attack, range, size, sight, speed, accuracy, reload, reload_time, x, y, order):
+        self.name =name 
         self.hp = hp                            # =0 si l'unite est morte
         self.team = team                        # choix de l'equipe
-        self.armure = armure
-        self.attaque = attaque
-        self.portee = portee
+        self.armor = armor
+        self.attack = attack
+        self.range = range
         self.size = size                        # taille de l'unite
-        self.vue = vue                          # Distance champ de vision
-        self.vitesse = vitesse                  # vitesse de deplacement
-        self.precision = precision
-        self.rechargement = rechargement        # avanncement du rechargement, peut attaquer a 0
-        self.temps_recharg = temps_recharg      # temps pour recharger
+        self.sight = sight                          # Distance champ de vision
+        self.speed = speed                  # vitesse de deplacement
+        self.accuracy = accuracy
+        self.reload = reload        # avanncement du rechargement, peut attaquer a 0
+        self.reload_time = reload_time      # temps pour recharger
         self.x = x
         self.y = y
         self.order = order
 
-    def vivant(self):
+    def alive(self):
         return (self.hp > 0)
     
-    def peut_attaquer(self):
-        if self.rechargement == 0:
-            self.attaquer
+    def can_attack(self):
+        if self.reload == 0:
+            self.attack()
         else :
-            self.recharger
+            self.reload()
     
-    def recharger(self):
-        if self.rechargement == 0:
+    def update_reload(self):
+        if self.reload == 0:
             return None
         else :
-            self.rechargement -= 1
+            self.reload -= 1
     
-    def attaquer(self):
+    def attack(self):
         pass
 
-    def deplacer(self):
+    def move(self):
         pass
 
 
 class Archer(Unit):
     def __init__(self, team, x, y):
-        super().__init__(nom = "Archer", team = team, hp = 30, armure = 0, attaque = 4, portee = 4, size = , vue = 6, vitesse = 0.96, precision = 0.8, rechargement = 2.0, x = x, y = y, order = )
+        super().__init__(name = "Archer", team = team, hp = 30, armor = 0, attack = 4, range = 4, size = , sight = 6, speed = 0.96, accuracy = 0.8, reload_time = 2.0, x = x, y = y, order = )
 
 class Knight(Unit):
     def __init__(self, team, x, y):
-        super().__init__(nom = "Knight", team = team, hp = 100, armure = 2, attaque = 10, portee = 0, size = , vue = 4, vitesse = 1.35, precision = 1.0, rechargement = 1.8, x = x, y = y, order =  )
+        super().__init__(name = "Knight", team = team, hp = 100, armor = 2, attack = 10, range = 0, size = , sight = 4, speed = 1.35, accuracy = 1.0, reload_time = 1.8, x = x, y = y, order =  )
 
 class Pikeman(Unit):
     def __init__(self, team, x, y):
-        super().__init__(nom = "Pikeman", team = team, hp = 55, armure = 0, attaque = 4, portee = 0, size = , vue = 4, vitesse = 1.0, precision = 1.0, rechargement = 3.0, x = x, y = y, order = )
+        super().__init__(name = "Pikeman", team = team, hp = 55, armor = 0, attack = 4, range = 0, size = , sight = 4, speed = 1.0, accuracy = 1.0, reload_time = 3.0, x = x, y = y, order = )
