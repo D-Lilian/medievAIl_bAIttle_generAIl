79277b5a93979e64da9e4baf214a97d4d8ae75b7
tom-bourneil <bourneiltom1@gmail.com>
2025-11-18 15:56:02 +0100

    Refactor Unit class and add unit types

--- create_3_types_of_units.py
+++ create_3_types_of_units.py
@@ -5,20 +5,20 @@ class Unit:
     
     def __init__(self, name, team, hp, armor, attack, range, size, sight, speed, accuracy, reload, reload_time, x, y, order):
         self.name =name 
-        self.hp = hp                            # =0 si l'unite est morte
-        self.team = team                        # choix de l'equipe
-        self.armor = armor
-        self.attack = attack
-        self.range = range
-        self.size = size                        # taille de l'unite
-        self.sight = sight                          # Distance champ de vision
+        self.hp = hp                        # =0 si l'unite est morte
+        self.team = team                    # choix de l'equipe
+        self.armor = armor                  # valeur de l'armure
+        self.attack = attack                # nombre de degats infliges
+        self.range = range                  # portee de l'unite
+        self.size = size                    # taille de l'unite
+        self.sight = sight                  # Distance champ de vision
         self.speed = speed                  # vitesse de deplacement
-        self.accuracy = accuracy
-        self.reload = reload        # avanncement du rechargement, peut attaquer a 0
+        self.accuracy = accuracy            # precision de l'unite
+        self.reload = reload                # avanncement du rechargement, peut attaquer a 0
         self.reload_time = reload_time      # temps pour recharger
-        self.x = x
-        self.y = y
-        self.order = order
+        self.x = x                          # coordonnee en X
+        self.y = y                          # coordonnee en Y
+        self.order = order                  # ordres donnees par le gerenal
 
     def alive(self):
         return (self.hp > 0)
@@ -27,29 +27,23 @@ class Unit:
         if self.reload == 0:
             self.attack()
         else :
-            self.reload()
+            self.update_reload()
     
-    def update_reload(self):
+    def update_reload(self, t):
         if self.reload == 0:
             return None
         else :
-            self.reload -= 1
-    
-    def attack(self):
-        pass
-
-    def move(self):
-        pass
+            self.reload -= t # !!! verifier la valeur selon le nb de tics par seconde !!!
 
 
 class Archer(Unit):
     def __init__(self, team, x, y):
-        super().__init__(name = "Archer", team = team, hp = 30, armor = 0, attack = 4, range = 4, size = , sight = 6, speed = 0.96, accuracy = 0.8, reload_time = 2.0, x = x, y = y, order = )
+        super().__init__(name = "Archer", team = team, hp = 30, armor = 0, attack = 4, range = 4, size = 1, sight = 6, speed = 0.96, accuracy = 0.8, reload_time = 2.0, x = x, y = y)
 
 class Knight(Unit):
     def __init__(self, team, x, y):
-        super().__init__(name = "Knight", team = team, hp = 100, armor = 2, attack = 10, range = 0, size = , sight = 4, speed = 1.35, accuracy = 1.0, reload_time = 1.8, x = x, y = y, order =  )
+        super().__init__(name = "Knight", team = team, hp = 100, armor = 2, attack = 10, range = 0, size = 1, sight = 4, speed = 1.35, accuracy = 1.0, reload_time = 1.8, x = x, y = y)
 
 class Pikeman(Unit):
     def __init__(self, team, x, y):
-        super().__init__(name = "Pikeman", team = team, hp = 55, armor = 0, attack = 4, range = 0, size = , sight = 4, speed = 1.0, accuracy = 1.0, reload_time = 3.0, x = x, y = y, order = )
+        super().__init__(name = "Pikeman", team = team, hp = 55, armor = 0, attack = 4, range = 0, size = 1, sight = 4, speed = 1.0, accuracy = 1.0, reload_time = 3.0, x = x, y = y)
