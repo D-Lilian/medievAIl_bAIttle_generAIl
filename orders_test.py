ddbf61884270361b11cb385d45a2ba97da407163
Votre Nom <streaburn@protonmail.com>
2025-12-02 15:56:15 +0100

    Added test with test_add_with_max_priority

--- orders_test.py
+++ orders_test.py
@@ -19,6 +19,24 @@ def test_add_and_get():
     assert om.Get(1) is o2
     assert om.Get(2) is None
 
+def test_add_with_max_priority():
+    om = OrderManager()
+    o1 = DummyOrder("A")
+    o2 = DummyOrder("B")
+    o3 = DummyOrder("C")
+
+    om.Add(o1, 0)
+    om.AddMaxPriority(o2) # Should be priority 1
+    om.AddMaxPriority(o3) # Should be priority 2
+
+    assert om.Get(0) is o1
+    assert om.Get(1) is o2
+    assert om.Get(2) is o3
+
+    om_empty = OrderManager()
+    o4 = DummyOrder("D")
+    om_empty.AddWithMaxPriority(o4) # Should be priority 0
+    assert om_empty.Get(0) is o4
 
 def test_add_priority_conflict():
     om = OrderManager()
