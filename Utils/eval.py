abecac24df4fe9118ad27cf6acdddf89d085f1e6
brice <brice.bourret.26@gmail.com>
2025-12-10 10:26:20 +0100

    Add eval.py controller model. executing command and managing scenarios.

--- Utils/eval.py
+++ Utils/eval.py
@@ -2,14 +2,14 @@ from ParseCLI import args
 from Model.scenarios.Scenario import Scenario
 from Controller.SimulationController import SimulationController
 # importer le cotroleur de lilian
-
+from Controller.simulation_controller import *
 
 def run(args):
     print(f"Running scenario: {args.scenario} with AIs: {args.ai1} vs {args.ai2}")
     # Appel de la fonction correspondant
     if args.t == "None":
         print("No terminal view selected.")
-        scenario = Scenario(
+        game = Scenario(
             units= #je ne sais pas qce qu'il a mis pour les unités
             units_a=50,
             units_b=50,
@@ -18,11 +18,27 @@ def run(args):
             size_x=120,
             size_y=120
         )
-        start_simulation(self,args.scenario):
+        start_simulation(game)
+
 
         # appeler le controleur en unlocked mode
     elif args.t == "-t":
         print("Terminal view enabled.")
+        from Controller.terminal_controller import TerminalController
+        from Model.scenario import Scenario
+        from Model.units import Knight, Pikeman, Team
+
+        units, units_a, units_b = [], [], []
+        u1 = Knight(Team.A, 20, 20);
+        units.append(u1);
+        units_a.append(u1)
+        u2 = Pikeman(Team.B, 95, 20);
+        units.append(u2);
+        units_b.append(u2)
+
+        scenario = Scenario(units, units_a, units_b, None, None, size_x=120, size_y=40)
+        controller = TerminalController(scenario, tick_speed=20)
+        controller.run()
 
         # appeler le controleur sans unlocked mode
 
@@ -44,7 +60,15 @@ def save(args):
 
 def Lanchester(args):
     #pas d ecran terminal
-
+    scenario = Scenario(
+        units=  # je ne sais pas qce qu'il a mis pour les unités
+        units_a = 50,
+    units_b = 50,
+    general_a = args.ai1,
+    general_b = args.ai2,
+    size_x = 120,
+    size_y = 120
+    )
 def main():
 
 
